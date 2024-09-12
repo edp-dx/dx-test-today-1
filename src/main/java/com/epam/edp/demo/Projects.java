c194aded54c402cd050399976da5484f30e3dba6
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:25 +0200

    Create Projects class

---
+++ src/main/java/com/epam/edp/demo/Projects.java
@@ -0,0 +1,17 @@
+package com.epam.edp.demo;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class Projects {
+    private UUID code;
+    private String fileName;
+    private ProjectMainType mainType;
+    private ProjectSubType subType;
+    private String assortmentCategory;
+    private Timestamp createdOn;
+    private Timestamp deadline;
+    private Timestamp inEffectFrom;
+
+    // Getters and Setters
+}
