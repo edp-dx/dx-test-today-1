f382d12c3fd5a81b1585ce4dc80156d367fe479b
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:32 +0200

    Create Products class

---
+++ src/main/java/com/epam/edp/demo/Products.java
@@ -0,0 +1,12 @@
+package com.epam.edp.demo;
+
+import java.util.UUID;
+
+public class Products {
+    private UUID id;
+    private UUID supplierId;
+    private String assortmentGroup;
+    private String shortDescription;
+
+    // Getters and Setters
+}
