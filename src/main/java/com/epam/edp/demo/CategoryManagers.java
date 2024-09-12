231064643bcb78eeafc4a0c8feb218db82fcd4c3
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:37 +0200

    Create CategoryManagers class

---
+++ src/main/java/com/epam/edp/demo/CategoryManagers.java
@@ -0,0 +1,11 @@
+package com.epam.edp.demo;
+
+import java.util.UUID;
+
+public class CategoryManagers {
+    private UUID id;
+    private String email;
+    private String assortmentCategory;
+
+    // Getters and Setters
+}
