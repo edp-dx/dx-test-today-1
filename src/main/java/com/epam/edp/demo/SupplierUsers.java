6a10d94ed0e2da5b0c934c38984d6af55e50d9be
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:38 +0200

    Create SupplierUsers class

---
+++ src/main/java/com/epam/edp/demo/SupplierUsers.java
@@ -0,0 +1,11 @@
+package com.epam.edp.demo;
+
+import java.util.UUID;
+
+public class SupplierUsers {
+    private UUID id;
+    private UUID supplierId;
+    private String email;
+
+    // Getters and Setters
+}
