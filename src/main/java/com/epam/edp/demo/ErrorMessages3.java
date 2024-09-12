73512d8c4432d3e2b704d3083f0f3c34bfaf3a5d
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:49 +0200

    Create ErrorMessages3 class

---
+++ src/main/java/com/epam/edp/demo/ErrorMessages3.java
@@ -0,0 +1,11 @@
+package com.epam.edp.demo;
+
+import java.util.UUID;
+
+public class ErrorMessages3 {
+    private UUID errorCode;
+    private ErrorMessages3Language language;
+    private String text;
+
+    // Getters and Setters
+}
