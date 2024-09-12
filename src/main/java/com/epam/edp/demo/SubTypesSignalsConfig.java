e10e14b3d3236a6d3c9380b6b84028a0ba774f98
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:19 +0200

    Create SubTypesSignalsConfig class

---
+++ src/main/java/com/epam/edp/demo/SubTypesSignalsConfig.java
@@ -0,0 +1,10 @@
+package com.epam.edp.demo;
+
+import java.util.UUID;
+
+public class SubTypesSignalsConfig {
+    private UUID subTypeId;
+    private UUID signalsConfigId;
+
+    // Getters and Setters
+}
