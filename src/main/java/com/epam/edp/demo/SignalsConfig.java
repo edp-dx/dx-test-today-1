3a4fd93b9187e5066243eff38bc966c77a6ee9cb
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:14 +0200

    Create SignalsConfig class

---
+++ src/main/java/com/epam/edp/demo/SignalsConfig.java
@@ -0,0 +1,14 @@
+package com.epam.edp.demo;
+
+import java.util.UUID;
+
+public class SignalsConfig {
+    private UUID id;
+    private SignalType signalType;
+    private int daysPriorToDeadline;
+    private String timeOfTheDay;
+    private int numberOfRetries;
+    private int retryInterval;
+
+    // Getters and Setters
+}
