36aa83667c0179d8014fe6016ce6a0a40178837e
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:30 +0200

    Create Errors class

---
+++ src/main/java/com/epam/edp/demo/Errors.java
@@ -0,0 +1,14 @@
+package com.epam.edp.demo;
+
+import java.util.UUID;
+
+public class Errors {
+    private UUID code;
+    private String initialErrorMessage;
+    private String enrichedErrorMessageEnglish;
+    private String enrichedErrorMessageDutch;
+    private String attribute;
+    private ErrorReceiverType receiverType;
+
+    // Getters and Setters
+}
