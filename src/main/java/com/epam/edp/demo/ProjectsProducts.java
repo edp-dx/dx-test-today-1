4674415b784e2040afdf0f9626427bd2817178ac
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:35 +0200

    Create ProjectsProducts class

---
+++ src/main/java/com/epam/edp/demo/ProjectsProducts.java
@@ -0,0 +1,13 @@
+package com.epam.edp.demo;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class ProjectsProducts {
+    private UUID projectId;
+    private UUID productId;
+    private ProductStatus status;
+    private Timestamp updatedOn;
+
+    // Getters and Setters
+}
