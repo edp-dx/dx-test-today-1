203fd8c019344ddce172fb586687af4185e11310
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:26 +0200

    Create ValidationsErrors class

---
+++ src/main/java/com/epam/edp/demo/ValidationsErrors.java
@@ -0,0 +1,16 @@
+package com.epam.edp.demo;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class ValidationsErrors {
+    private UUID projectCode;
+    private UUID supplierId;
+    private UUID productId;
+    private UUID errorCode;
+    private String initialErrorMessage;
+    private Timestamp createdOn;
+    private Timestamp solvedOn;
+
+    // Getters and Setters
+}
