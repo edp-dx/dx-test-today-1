ba02cb74312d3ed7e6706a4a7d72e17e19b11c5b
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:44 +0200

    Create ErrorMessages2 class

---
+++ src/main/java/com/epam/edp/demo/ErrorMessages2.java
@@ -0,0 +1,12 @@
+package com.epam.edp.demo;
+
+import java.util.UUID;
+
+public class ErrorMessages2 {
+    private UUID id;
+    private UUID errorCode;
+    private UUID languageId;
+    private String text;
+
+    // Getters and Setters
+}
