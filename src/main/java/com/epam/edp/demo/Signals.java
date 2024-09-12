15ef4d1d76be99ede56b2aba385b308093f29f3f
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:40 +0200

    Create Signals class

---
+++ src/main/java/com/epam/edp/demo/Signals.java
@@ -0,0 +1,15 @@
+package com.epam.edp.demo;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class Signals {
+    private UUID id;
+    private UUID projectCode;
+    private Timestamp deliveryTime;
+    private boolean delivered;
+    private SignalType type;
+    private boolean invalid;
+
+    // Getters and Setters
+}
