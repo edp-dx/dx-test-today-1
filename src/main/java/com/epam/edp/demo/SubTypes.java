0b2a4fa9ee4f3e06b94288df616e97312b102389
Andrey Trubitsyn <andtrub@gmail.com>
2024-09-12 10:29:17 +0200

    Create SubTypes class

---
+++ src/main/java/com/epam/edp/demo/SubTypes.java
@@ -0,0 +1,11 @@
+package com.epam.edp.demo;
+
+import java.util.UUID;
+
+public class SubTypes {
+    private UUID id;
+    private String name;
+    private MainTypes mainType;
+
+    // Getters and Setters
+}
